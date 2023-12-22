c1525878eb9dbef9c9c2f46133adc01300e5f25d
Kubilay Cem KARAHAN <kubilaycem@yandex.com>
2023-12-22 23:45:53 +0300

    Added Custom Exception Middleware

---
+++ VirtualPetCare.API/Middlewares/CustomExceptionMiddleware.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+using System.Net;
+using VirtualPetCare.Service.Exceptions;
+
+namespace VirtualPetCare.API.Middlewares
+{
+    public class CustomExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public CustomExceptionMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+
+                context.Response.ContentType = "application/json";
+
+                var statusCode = ex switch
+                {
+                    ClientSideException => HttpStatusCode.BadRequest,
+                    NotFoundException => HttpStatusCode.NotFound,
+                    _ => HttpStatusCode.InternalServerError
+                };
+
+                context.Response.StatusCode = (int)statusCode;
+
+                await context.Response.WriteAsJsonAsync(ex.Message);
+            }
+        }
+    }
+}
+
