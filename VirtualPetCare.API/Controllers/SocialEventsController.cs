7822a9eee8ab994bc8647133216b1e8532e52efd
Kubilay Cem KARAHAN <kubilaycem@yandex.com>
2023-12-23 00:39:13 +0300

    Added API Version

--- VirtualPetCare.API/Controllers/SocialEventsController.cs
+++ VirtualPetCare.API/Controllers/SocialEventsController.cs
@@ -5,7 +5,7 @@ using VirtualPetCare.Core.Services;
 
 namespace VirtualPetCare.API.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/v1/[controller]")]
     [ApiController]
     public class SocialEventsController : ControllerBase
     {
